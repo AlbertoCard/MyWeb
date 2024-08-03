abd0eda1a34eb8a20939ad6c25a76885634c861b
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2024-08-03 15:22:48 -0700

    rest y spred

--- JS/Notas/Funciones.js
+++ JS/Notas/Funciones.js
@@ -33,6 +33,31 @@ const multiplicacion = function(a, b) {
 }
 console.log(multiplicacion(5, 3));
 
+// parametros rest
+
+function sumar(a, b, ...numeros) {
+    let resultado = a + b;
+    numeros.forEach(function(numero) {
+        resultado += numero;
+    });
+    return resultado;
+}
+
+console.log(sumar(5, 3)); // 8
+console.log(sumar(5, 3, 2)); // 10
+console.log(sumar(5, 3, 2, 4)); // 14
+console.log(sumar(5, 3, 2, 4, 6)); // 20
+
+// parametros por defecto
+function saludar(nombre = "Mundo") {
+    console.log("Hola " + nombre);
+}
+
+// operador spread
+const arr1 = [1, 2, 3, 4, 5], arr2 = [6, 7, 8, 9, 10];
+const arr3 = [arr1, arr2]; // [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
+const arr4 = [...arr1, ...arr2]; // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 
 
 
