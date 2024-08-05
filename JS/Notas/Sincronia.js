2f1997c2e09bb7f28feb840113d7ad82fc49e205
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2024-08-04 19:42:41 -0700

    promesas

--- JS/Notas/Sincronia.js
+++ JS/Notas/Sincronia.js
@@ -68,3 +68,51 @@ cuadradoCallback(0, (value, result) => {
         });
     });
 });
+
+// promesas
+// Una promesa es un objeto que representa la terminación o el fracaso de una operación asíncrona.
+// Las promesas tienen tres estados: pendiente, resuelta o rechazada.
+// Las promesas se crean con el constructor Promise.
+
+function cuadradoPromise(value) {
+    if (typeof value !== 'number') {
+        return Promise.reject(`Error, el valor "${value}" ingresado no es un número`);
+    }
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                value,
+                result: value * value
+            });
+        }, 0 | Math.random() * 1000);
+    });
+}
+
+cuadradoPromise(0)
+    .then(obj => {
+        console.log('Inicio Promise');
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+        return cuadradoPromise(1);
+    })
+    .then(obj => {
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+        return cuadradoPromise(2);
+    })
+    .then(obj => {
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+        return cuadradoPromise(3);
+    })
+    .then(obj => {
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+        return cuadradoPromise(4);
+    })
+    .then(obj => {
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+        return cuadradoPromise(5);
+    })
+    .then(obj => {
+        console.log(`Promise: ${obj.value}, ${obj.result}`);
+        console.log('Fin Promise');
+    })
+    .catch(err => console.error(err));
+    
