35e61ea3f269b6ce8e00c32061567eecab4a535f
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2024-08-07 19:55:39 -0700

    uso de emergentes

--- JS/Notas/Emergentes.js
+++ JS/Notas/Emergentes.js
@@ -10,3 +10,10 @@ else{
 var aviso = prompt("Introduce tu nombre"); // Muestra un mensaje emergente con un campo de texto
 console.log(aviso); // Muestra el valor introducido en el campo de texto
 
+
+
+
+// uso de ventanas emergentes
+const abrir = document.getElementById('abrir-ventana'),
+        cerrar = document.getElementById('cerrar-ventana'),
+        imprimir = document.getElementById('imprimir-ventana');
