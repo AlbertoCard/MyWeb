5a9e7efa86e54cf1861defef3fabbf6b7037a6d0
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2024-08-09 23:24:15 -0700

    E_4 - Funcionalidad del reloj

--- JS/E_4/index_dom.js
+++ JS/E_4/index_dom.js
@@ -1,7 +1,9 @@
 import menu_hamburguesa from "./menu_hamburguesa.js";
+import { digitalClock, alarm } from "./reloj.js";
 
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
     menu_hamburguesa(".panel-btn", ".panel", ".menu a");
+    digitalClock("#reloj", "#activar-reloj", "#detener-reloj");
 });
