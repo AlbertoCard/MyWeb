8c9e9b70255c6bc6f8f65774ca02b23f892dee50
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2024-08-15 18:50:59 -0700

    E_4 - Elementos responsive

--- JS/E_4/index_dom.js
+++ JS/E_4/index_dom.js
@@ -4,6 +4,7 @@ import { shortcuts, moveBall } from "./teclado.js";
 import countdown from "./cuenta_regresiva.js";
 import scrollTopButton from "./boton_scroll.js";
 import darkTheme from "./tema_oscuro.js";
+import responsiveMedia from "./objeto_responsive.js";
 
 const d = document;
 
@@ -13,6 +14,18 @@ d.addEventListener("DOMContentLoaded", (e) => {
     alarm("assets/fire-alarm-33770.mp3", "#activar-alarma", "#detener-alarma");
     countdown("countdown", "Sep 19, 2024 00:00:00", "Feliz Cumpleaños!! 🎂🎉");
     scrollTopButton(".scroll-top-btn");
+    responsiveMedia(
+        "youtube", 
+        "(min-width: 1024px)", 
+        `<a href="https://www.youtube.com/watch?v=Z47tQaJ39tA" target="_blank" rel="noopener">Ver video</a>`,
+        `<iframe width="560" height="315" src="https://www.youtube.com/embed/Z47tQaJ39tA?si=wBZa1IpioqftZbLS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+    );
+    responsiveMedia(
+        "maps", 
+        "(min-width: 1024px)", 
+        `<a href="https://goo.gl/maps/5o1zQJZ2HJ6mZz9e6" target="_blank" rel="noopener">Ver mapa</a>`,
+        `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3622.050370372235!2d-107.37906582463125!3d24.79372867797288!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86bcd781d3ffa737%3A0xdf51d5881ac1823e!2sLa%20Torre%20Pizza!5e0!3m2!1ses-419!2smx!4v1723772085101!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+    );
 });
 
 d.addEventListener("keydown", (e) => {
