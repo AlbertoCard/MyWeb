b1293634326ff6fe73f77e7e55e8c75751d21911
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2024-08-15 23:29:53 -0700

    E_4 - sorteo

---
+++ JS/E_4/sorteo.js
@@ -0,0 +1,17 @@
+const d = document;
+
+export default function sorteo(btn, selector) {
+    const getWinner = (selector) => {
+        const $players = d.querySelectorAll(selector),
+            random = Math.floor(Math.random() * $players.length),
+            winner = $players[random];
+        return `${winner.textContent}`;
+    };
+
+    d.addEventListener("click", (e) => {
+        if (e.target.matches(btn)){
+            let result = getWinner(selector);
+            alert(`El ganador es: ${result}`);
+        }
+    });
+}
