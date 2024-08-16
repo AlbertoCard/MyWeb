22147066c892ac015d5457e4ad6859eacf53c14b
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2024-08-15 18:05:30 -0700

    E_4 - Dark mode con local storage

--- JS/E_4/index_dom.js
+++ JS/E_4/index_dom.js
@@ -13,10 +13,12 @@ d.addEventListener("DOMContentLoaded", (e) => {
     alarm("assets/fire-alarm-33770.mp3", "#activar-alarma", "#detener-alarma");
     countdown("countdown", "Sep 19, 2024 00:00:00", "Feliz Cumpleaños!! 🎂🎉");
     scrollTopButton(".scroll-top-btn");
-    darkTheme(".dark-theme-btn", "dark-mode");
 });
 
 d.addEventListener("keydown", (e) => {
     shortcuts(e);
     moveBall(e, ".ball", ".stage");
 });
+
+
+darkTheme(".dark-theme-btn", "dark-mode");
