88c838f57043995d055365916c0b819699121e1f
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2024-08-17 21:16:06 -0700

    Ajax - Axios

--- JS/AJAX/ajax.js
+++ JS/AJAX/ajax.js
@@ -89,3 +89,28 @@
 
     getData()
 })();
+
+(() => {
+    const $axios = document.getElementById("axios"),
+        $fragment = document.createDocumentFragment();
+
+    axios.get("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+        let json = res.data;
+
+        json.forEach((el) => {
+            const $li = document.createElement("li");
+            $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+            $fragment.appendChild($li);
+        });
+
+        $axios.appendChild($fragment);
+    })
+    .catch((err) => {
+        let message = err.response.statusText || "Ocurrio un error";
+        $axios.innerHTML = `Error ${err.response.status}: ${message}`;
+    })
+    .finally(() => {
+        console.log("Esto se ejecutara independientemente del resultado de la promesa Axios");
+    });
+})();
