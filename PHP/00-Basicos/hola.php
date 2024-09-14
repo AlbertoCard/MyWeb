bc0b5337b428523abd58191c31bc7fbfe77c3ec8
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2024-09-13 23:47:48 -0700

    Comenzando php

---
+++ PHP/00-Basicos/hola.php
@@ -0,0 +1,20 @@
+<?php
+echo "Hola Mundo!";
+
+echo "<br>";
+echo "<h1>Esto es un encabezado</h1>";
+
+//variables
+$nombre = "Alberto";
+$apellido = "Cardenas";
+
+// para concatenar se usa el punto
+echo "Mi nombre es: " . $nombre . " " . $apellido;
+echo "<br/>";
+
+$num1 = 10;
+$num2 = 20;
+
+echo "La suma de " . $num1 . " + " . $num2 . " = " . ($num1 + $num2);
+
+?>
