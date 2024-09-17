e15a621b99fae8cb7f748d4f3a8fafc4a6c0e2d9
AlbertoCard <alberto.cardenaszazueta@gmail.com>
2024-09-16 23:51:23 -0700

    php - validacion de datos

--- PHP/02-Validacion-datos/validar-datos.php
+++ PHP/02-Validacion-datos/validar-datos.php
@@ -1,3 +1,14 @@
 <?php
+    $nombre = "admin";
+    $password = "123";
 
+    if(isset($_GET["enviar_hdn"])){
+        if($nombre == $_GET["name_txt"] && $password == $_GET["password_txt"]){
+            echo("Usuario:".$_GET["name_txt"]."<br>");
+            echo("Password: ".$_GET["password_txt"]."<br>");
+            echo("Sexo: ".$_GET["sexo_rdo"]);
+        } else{
+            echo("Location: formulario.php?err=si");
+        }
+    }
 ?>
